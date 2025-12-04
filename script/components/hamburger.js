307969e6b87dd214997d6f4c4e7f637a4d787b1e
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-12-04 11:09:45 +0100

    feat: Implement responsive hamburger menu with overlay
    - Added hamburger button and menu to the header for mobile navigation.
    - Created overlay for the hamburger menu to enhance user experience.
    - Implemented JavaScript functionality to toggle the menu and overlay visibility.
    - Styled the hamburger menu and overlay for better aesthetics and usability.
    - Adjusted header styles for mobile view to hide default navigation links.
    - Included new icons for hamburger and close states.

---
+++ script/components/hamburger.js
@@ -0,0 +1,43 @@
+screenWidth = window.innerWidth;
+const hamburgerIcon = document.getElementById("hamburger-button");
+const hamburgerOverlay = document.getElementById("hamburger-overlay");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+
+let isActive = false
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    screenWidth = window.innerWidth;
+    const hamburgerIcon = document.getElementById("hamburger-button");
+    const hamburgerOverlay = document.getElementById("hamburger-overlay");
+    const hamburgerMenu = document.getElementById("hamburger-menu");
+
+    if (screenWidth <= 768) {
+        hamburgerIcon.style.display = "block";
+    }
+
+    hamburgerIcon.addEventListener("click", function () {
+    isActive = !isActive;
+
+    // fade out
+    hamburgerIcon.style.transition = "opacity 0.3s ease";
+    hamburgerIcon.style.opacity = 0;
+
+        setTimeout(() => {
+            if (isActive) {
+                hamburgerMenu.classList.add("active");
+                hamburgerOverlay.classList.add("active");
+                hamburgerIcon.src = "../../assets/icon/cross.png";
+            } else {
+                hamburgerMenu.classList.remove("active");
+                hamburgerOverlay.classList.remove("active");
+                hamburgerIcon.src = "../../assets/icon/hamburger.png";
+            }
+
+            // fade in
+            hamburgerIcon.style.opacity = 1;
+        }, 100); // correspond à la durée du fade
+    });
+
+
+});
