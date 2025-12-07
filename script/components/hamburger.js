ff0df75c3db861cf727af5f7477d5280f2386f61
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-12-07 22:24:17 +0100

    Fix: Refactor hamburger menu functionality and improve overlay visibility

--- script/components/hamburger.js
+++ script/components/hamburger.js
@@ -1,20 +1,13 @@
-screenWidth = window.innerWidth;
-const hamburgerIcon = document.getElementById("hamburger-button");
-const hamburgerOverlay = document.getElementById("hamburger-overlay");
-const hamburgerMenu = document.getElementById("hamburger-menu");
+let isActive = false;
 
-let isActive = false
+document.addEventListener("DOMContentLoaded", () => {
+    setupHamburger();
+});
 
-document.addEventListener("DOMContentLoaded", function() {
-
-    screenWidth = window.innerWidth;
+function setupHamburger() {
     const hamburgerIcon = document.getElementById("hamburger-button");
     const hamburgerOverlay = document.getElementById("hamburger-overlay");
     const hamburgerMenu = document.getElementById("hamburger-menu");
-
-    if (screenWidth <= 768) {
-        hamburgerIcon.style.display = "block";
-    }
 
     hamburgerIcon.addEventListener("click", function () {
         isActive = !isActive;
@@ -23,23 +16,21 @@ document.addEventListener("DOMContentLoaded", function() {
         hamburgerIcon.style.transition = "opacity 0.3s ease";
         hamburgerIcon.style.opacity = 0;
 
-            setTimeout(() => {
-                if (isActive) {
-                    hamburgerMenu.classList.add("active");
-                    hamburgerOverlay.classList.add("active");
-                    hamburgerIcon.src = "../../assets/icon/cross.png";
-                    document.body.style.overflow = "hidden";
-                } else {
-                    hamburgerMenu.classList.remove("active");
-                    hamburgerOverlay.classList.remove("active");
-                    hamburgerIcon.src = "../../assets/icon/hamburger.png";
-                    document.body.style.overflow = "auto";
-                }
+        setTimeout(() => {
+            if (isActive) {
+                hamburgerMenu.classList.add("active");
+                hamburgerOverlay.classList.add("active");
+                hamburgerIcon.src = "../../assets/icon/cross.png";
+                document.body.style.overflow = "hidden";
+            } else {
+                hamburgerMenu.classList.remove("active");
+                hamburgerOverlay.classList.remove("active");
+                hamburgerIcon.src = "../../assets/icon/hamburger.png";
+                document.body.style.overflow = "auto";
+            }
 
-                // fade in
-                hamburgerIcon.style.opacity = 1;
-            }, 100); // correspond à la durée du fade
-        });
-
-
-});
+            // fade in
+            hamburgerIcon.style.opacity = 1;
+        }, 100);
+    });
+}
