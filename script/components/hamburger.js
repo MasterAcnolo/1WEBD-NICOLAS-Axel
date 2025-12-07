42cb1f70c6569ef27aa2db41cea1c906b5c57313
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-12-07 13:13:50 +0100

    fix: Hamburger Menu Indentation

--- script/components/hamburger.js
+++ script/components/hamburger.js
@@ -17,29 +17,29 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     hamburgerIcon.addEventListener("click", function () {
-    isActive = !isActive;
+        isActive = !isActive;
 
-    // fade out
-    hamburgerIcon.style.transition = "opacity 0.3s ease";
-    hamburgerIcon.style.opacity = 0;
+        // fade out
+        hamburgerIcon.style.transition = "opacity 0.3s ease";
+        hamburgerIcon.style.opacity = 0;
 
-        setTimeout(() => {
-            if (isActive) {
-                hamburgerMenu.classList.add("active");
-                hamburgerOverlay.classList.add("active");
-                hamburgerIcon.src = "../../assets/icon/cross.png";
-                document.body.style.overflow = "hidden";
-            } else {
-                hamburgerMenu.classList.remove("active");
-                hamburgerOverlay.classList.remove("active");
-                hamburgerIcon.src = "../../assets/icon/hamburger.png";
-                document.body.style.overflow = "auto";
-            }
+            setTimeout(() => {
+                if (isActive) {
+                    hamburgerMenu.classList.add("active");
+                    hamburgerOverlay.classList.add("active");
+                    hamburgerIcon.src = "../../assets/icon/cross.png";
+                    document.body.style.overflow = "hidden";
+                } else {
+                    hamburgerMenu.classList.remove("active");
+                    hamburgerOverlay.classList.remove("active");
+                    hamburgerIcon.src = "../../assets/icon/hamburger.png";
+                    document.body.style.overflow = "auto";
+                }
 
-            // fade in
-            hamburgerIcon.style.opacity = 1;
-        }, 100); // correspond à la durée du fade
-    });
+                // fade in
+                hamburgerIcon.style.opacity = 1;
+            }, 100); // correspond à la durée du fade
+        });
 
 
 });
