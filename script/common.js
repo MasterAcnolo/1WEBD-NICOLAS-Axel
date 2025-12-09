b9347649d4d90e0f124c4ad7a9b80eaefa0ca2e2
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-12-09 19:58:37 +0100

    Fix: Add comment to clarify function purpose for clearing search.html

--- script/common.js
+++ script/common.js
@@ -3,6 +3,7 @@ AOS.init();
 
 const searchLinks = document.querySelectorAll('a.link');
 
+/* Function to clear search.html (new research)*/
 searchLinks.forEach(link => {
     if (link.textContent.trim() === "Search") {
         link.addEventListener("click", () => {
